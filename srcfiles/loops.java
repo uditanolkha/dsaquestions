1b7c5021c0bd8f8210b9d884d10a227f00b3c7a1
Udita Nolkha <uditanolkha@gmail.com>
2024-08-27 20:44:24 +0530

    add loops file

---
+++ srcfiles/loops.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class loops{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //for loop
+        // System.out.println("ENTER THE UPPERBOUND :");
+        // int n = input.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     System.out.println(i);
+        // }
+
+        //while loop
+        // int i = 1;
+        // while(i<=5){
+        //     System.out.println(i);
+        //     i+=1;
+        // }
+
+        //do while
+        int i=1;
+        do{
+            System.out.println("hello world!");
+        }while(i!=1);
+    }
+}
