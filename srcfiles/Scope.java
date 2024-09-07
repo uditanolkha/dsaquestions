30e87272f6dcb23a8bed2f47cd0372f03d7e25b1
Udita Nolkha <uditanolkha@gmail.com>
2024-09-07 10:56:27 +0530

    second commit

--- srcfiles/Scope.java
+++ srcfiles/Scope.java
@@ -19,6 +19,13 @@ public class Scope {
         System.out.println("Local variable c = " + c);
         fun(2,3 ,76,89,90,78);
         varArgs(3,4,"hello","hii");
+        demo("nolkha","soni","jain");
+    }
+    static void demo(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+    static void demo(String ...v){
+        System.out.println(Arrays.toString(v));
     }
     static void fun(int ...v){
         System.out.println(Arrays.toString(v));
